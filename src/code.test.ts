656e79bfb69248f8621aebd61bc90b40389137d9
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-04-06 10:44:59 -0300

    chore: adicionando solução e teste unitário da quinta questão

--- src/code.test.ts
+++ src/code.test.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest';
-import { cortados, numeros } from './code.ts';
+import { numPares } from './code.ts';
 
 //describe('', () => {
-  // array de referência: [2, 4, 6, 2, 8, 9, 5]
-  it('Mostrar apenas os dois primeiros elementos de um array usando o método slice()', () => {
-    const sliced = [2, 4];
-    expect(sliced).toEqual(cortados);
+  // array de referência: [8, 3, 9, 5, 6, 12]
+  it('Mostrar os números pares de uma lista usando o método filter()', () => {
+    const pares = [8, 6, 12];
+    expect(pares).toEqual(numPares);
   });
 //});
 
--- src/code.ts
+++ src/code.ts
@@ -1,11 +1,12 @@
 /*
-4. Faça um programa que leia o array pegue apenas os dois primeiros elementos. Use o método SLICE da classe Array.
-4.1 Escreva um teste com o array [2,4,6,2,8,9,5]
+5. Faça um programa que leia o array e extrai os elementos pares. Use o método FILTER da classe Array, passando uma função arrow como parâmetro.
+5.1 Escreva um teste com o array [8, 3, 9, 5, 6, 12]
 */
 
-console.log(`Atividade 4: Mostrar apenas os 2 primeiros elementos da lista usando o método slice()\n\n`);
-export const numeros = [2, 4, 6, 2, 8, 9, 5];
-export const cortados = numeros.slice(0, 2);
+console.log(`Atividade 5: Mostrar os números pares de uma lista usando o método filter()\n\n`);
+const outrosNumeros = [8, 3, 9, 5, 6, 12];
 
-console.log(`Lista original: ${numeros}\n`);
-console.log(`Apenas os 2 primeiros números: ${cortados}`);
+console.log(`Lista original: ${outrosNumeros}\n`);
+export const numPares = outrosNumeros.filter((element) => element % 2 == 0 );
+
+console.log(`Apenas os números pares da lista: ${numPares}`);
