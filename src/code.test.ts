b84ed7335a0048811e02056ae7fc5bdb77571d1e
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-04-06 10:28:50 -0300

    chore: adicionando solução e teste unitário da segunda questão

--- src/code.test.ts
+++ src/code.test.ts
@@ -1,14 +1,10 @@
 import { describe, it, expect } from 'vitest';
-import { lista, listaAoQuadrado, listaAoQuadrado2 } from './code.ts';
+import { strings, formatado } from './code.ts';
 
-describe('Testes para elevar elementos ao quadrado', () => {
-  it('cada elemento do array elevado ao quadrado usando for tradicional', () => {
-    const esperado = [9, 25, 49, 9, 64, 81, 1]; // quadrados de [3,5,7,3,8,9,1]
-    expect(listaAoQuadrado).toEqual(esperado);
-  });
 
-  it('cada elemento do array elevado ao quadrado usando forEach', () => {
-    const esperado = [9, 25, 49, 9, 64, 81, 1]; // quadrados de [3,5,7,3,8,9,1]
-    expect(listaAoQuadrado2).toEqual(esperado);
-  });
+it('Teste para concatenar strings de um array usando o método join()', () => {
+  const concatenados = strings.join(' '); // strings = ['Arrays', 'com', 'TypeScript'];
+  expect(concatenados).toEqual(formatado);
 });
+
+
--- src/code.ts
+++ src/code.ts
@@ -1,25 +1,11 @@
 /*
-1. Faça um programa TypeScript que calcule o quadrado de cada elemento do array, use as seguintes estratégias:
-a. iterando com for simples
-b. iterando com forEach
-1.1 Escreve um teste com o array [3,5,7,3,8,9,1]
+2. Faça um Programa TypeScript que transforme o array, concatenando as strings com 1 (um) espaço (“ “). Utilize o método JOIN da classe Array, passando uma função arrow como parâmetro.
+2.1 Escreva um teste com o array [‘Arrays’, ‘com’, ‘TypeScript’]
 */
+console.log(`Atividade 2: Transformar um array através da concatenação das strings com 1 (um) espaço (" ") utilizando o método join()\n\n`);
+export const strings = ['Arrays', 'com', 'TypeScript'];
 
-console.log(`Atividade 1: Calcular o quadrado de cada elemento do array\n\n`);
-export const lista = [3, 5, 7, 3, 8, 9, 1];
-
-export const listaAoQuadrado = [...lista];
-
-export const listaAoQuadrado2 = [...lista];
-
-for (let i = 0; i < lista.length; ++i){
-    listaAoQuadrado[i] = listaAoQuadrado[i] ** 2;
-}
-
-listaAoQuadrado2.forEach((element, index, array) => {
-    array[index] = element ** 2;
-});
-
-console.log(`Lista original: ${lista}\n`);
-console.log(`Pelo for tradicional: ${listaAoQuadrado}\n`);
-console.log(`Pelo forEach: ${listaAoQuadrado2}`);
+export const concatenando = (lista: Array<string>) => lista.join(' ');
+export const formatado = concatenando(strings);
+console.log(`Lista original: ${strings}\n`);
+console.log(`Lista concatenada: ${formatado}`);
